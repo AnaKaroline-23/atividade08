1c4dabdb235b45184f052b55bde4fa5ef6e69e13
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-09 10:09:49 -0300

    conversão de reais para dólares

---
+++ atividade08.py
@@ -0,0 +1,9 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+real = float(input (" valor de conversão "))
+dolar = float(input(" valor do dolar "))
+valorconvertido = real / dolar 
+print((f" seu valor em dolar é: $ {valorconvertido}"))
+
+
+
+
